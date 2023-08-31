332ee3c63ea1a3a1472efbd9fa966ca22c538e8a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-31 15:22:22 -0600

    -- Finished previous notes, about to go over Declarative and Imperative with Meta

---
+++ MetaIntroToSwiftUI/DeclarativeVsImperativeNotes.swift
@@ -0,0 +1,8 @@
+//
+//  DeclarativeVsImperativeNotes.swift
+//  MetaIntroToSwiftUI
+//
+//  Created by Mitch Andrade on 8/31/23.
+//
+
+import Foundation
---
+++ MetaIntroToSwiftUI/NotesAppStructureAppProtocolSwiftUIArchitecture.swift
@@ -0,0 +1,19 @@
+//
+//  NotesAppStructureAppProtocolSwiftUIArchitecture.swift
+//  MetaIntroToSwiftUI
+//
+//  Created by Mitch Andrade on 8/31/23.
+//
+
+import Foundation
+
+// SwiftUI contains only 1 main app structure
+
+// Scene management is handle by the Scene
+/// Apple will adjust and handle all the heavy lifting for different hardware and devices for us
+/// The system will always decide the best way to show the user interface
+
+// Review
+/// The struct for handling views
+/// The preview provider for light UI 
+
